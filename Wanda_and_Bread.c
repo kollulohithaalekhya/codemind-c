8ed6412882daddf45aa2b224ce1f1735274a6bfd
kollulohithaalekhya <147791802+kollulohithaalekhya@users.noreply.github.com>
2023-10-20 11:56:00 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     int d=(b*c);     if(a>d)     {         printf("NO");     }     else     {         printf("YES");     } }

---
+++ Wanda_and_Bread.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    int d=(b*c);
+    if(a>d)
+    {
+        printf("NO");
+    }
+    else
+    {
+        printf("YES");
+    }
+}
