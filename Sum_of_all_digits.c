1a10abd99493a43299c878634745aa6a9fac1414
kollulohithaalekhya <147791802+kollulohithaalekhya@users.noreply.github.com>
2023-11-03 11:54:04 +0530

    #include<stdio.h> int main() {     int r,n,ds=0;     scanf("%d",&n);     while(n!=0)     {         r=n%10;         ds=ds+r;         n=n/10;     }     printf("%d",ds);
    }

---
+++ Sum_of_all_digits.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int r,n,ds=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        r=n%10;
+        ds=ds+r;
+        n=n/10;
+    }
+    printf("%d",ds);
+    
+}
