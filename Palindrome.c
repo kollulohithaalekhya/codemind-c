0b2eeac4a7ab492ef31b8aafadd9c103d7040533
kollulohithaalekhya <147791802+kollulohithaalekhya@users.noreply.github.com>
2023-11-11 00:01:03 +0530

    #include<stdio.h> int main() {     int r,a,n,i,t,rev=0;      scanf("%d",&n);      t=n;      for(i=1;n!=0;i++)      {       r=n%10;       n=n/10;      rev=rev*10+r;      }      if(t==rev)      {          printf("True");      }      else      {          printf("False");      } }

--- Palindrome.c
+++ Palindrome.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int r,n,i,t,rev=0;
-    scanf("%d",&n);
-    t=n;
-    for(i=1;n!=0;i++)
-    {
-        r=n%10;
-        rev=rev*10+r;
-        n=n/10;
-    }
-    if(t==rev)
-    {
-        printf("Palindrome");
-    }
-    else
-    {
-        printf("Not Palindrome");
-    }
+    int r,a,n,i,t,rev=0;
+     scanf("%d",&n);
+     t=n;
+     for(i=1;n!=0;i++)
+     {
+      r=n%10;
+      n=n/10;
+     rev=rev*10+r;
+     }
+     if(t==rev)
+     {
+         printf("True");
+     }
+     else
+     {
+         printf("False");
+     }
 }
