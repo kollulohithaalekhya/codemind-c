eacd63652e7b5d72d3c3535e71bf93a8c19310bf
kollulohithaalekhya <147791802+kollulohithaalekhya@users.noreply.github.com>
2023-12-10 21:20:12 +0530

    #include<stdio.h> int main() {     int n,m;     while(1)     {         scanf("%d",&n);         if(n==-1)         {             break;         }         m=n*n;     printf("%d ",m);     } }

---
+++ Infinite.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    while(1)
+    {
+        scanf("%d",&n);
+        if(n==-1)
+        {
+            break;
+        }
+        m=n*n;
+    printf("%d
+",m);
+    }
+}
