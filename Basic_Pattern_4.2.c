4fd1b4b72b837068218dcc8fbd871441d3f7bf93
kollulohithaalekhya <147791802+kollulohithaalekhya@users.noreply.github.com>
2023-12-31 21:08:55 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {         if(i==n || i==1 || i%2!=0)         printf("1 ");         else         printf("0 ");         }         printf(" ");     } }

---
+++ Basic_Pattern_4.2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+        if(i==n || i==1 || i%2!=0)
+        printf("1 ");
+        else
+        printf("0 ");
+        }
+        printf("
+");
+    }
+}
