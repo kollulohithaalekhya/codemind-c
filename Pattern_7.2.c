fbf17a5d96bc9bdc26f7f87e5c29e7b1dfffdc65
kollulohithaalekhya <147791802+kollulohithaalekhya@users.noreply.github.com>
2024-02-26 08:46:39 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=n-i;j++)         {             printf(" ");         }         for(int k=1;k<=i;k++)         {             printf("%d ",i);         }         printf(" ");     } }

---
+++ Pattern_7.2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(int k=1;k<=i;k++)
+        {
+            printf("%d ",i);
+        }
+        printf("
+");
+    }
+}
